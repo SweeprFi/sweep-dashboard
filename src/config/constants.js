ddb60391d33cad5efa742e9c12318b39f54ae30d
Cela Pablo <pablooandress9@gmail.com>
2023-10-18 10:17:07 -0300

    updates MarketMaker addresses

--- src/config/constants.js
+++ src/config/constants.js
@@ -112,9 +112,9 @@ const tokens = {
 
 const contracts = {
   marketMaker: {
-    1: '0x709d075147a10495e5c3bBF3dfc0c138F34C6E72',
+    1: '0xff368E106EA8782FaB6B2D4AD69739a60C66400E',
     5: '',
-    42161: '0x33A48e4aA79A66fc4f7061f5D9E274528C213029',
+    42161: '0x78326Ce3be64977658726EEdAd9A35de460E310A',
     421613: ''
   }
 }
