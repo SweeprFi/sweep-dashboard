ad4420c924facfee27327b9d9d057894ca087957
Cela Pablo <pablooandress9@gmail.com>
2023-11-24 15:09:14 -0300

    adds Base network

--- src/config/constants.js
+++ src/config/constants.js
@@ -7,6 +7,7 @@ import { ReactComponent as MediumIcon } from "@images/icons/medium.svg"
 import ethIcon from "@images/chains/ethereum.svg"
 import arbIcon from "@images/chains/arbitrum.svg"
 import optIcon from "@images/chains/optimism.svg"
+import baseIcon from "@images/chains/base.svg"
 
 import SweepLogo from "@images/logo.png"
 import SweeprLogo from "@images/icon_sweepr.png"
@@ -22,6 +23,7 @@ const networks = {
   1: 'mainnet',
   5: 'goerli',
   10: 'optimism',
+  8453: 'base',
   // 420: 'optimism goerli',
   42161: 'arbitrum',
   421613: 'arbiturm goerli',
@@ -32,6 +34,7 @@ const rpcLinks = {
   1: `https://eth-mainnet.alchemyapi.io/v2/${process.env.REACT_APP_ALCHEMY_KEY}`,
   10: `https://opt-mainnet.g.alchemy.com/v2/${process.env.REACT_APP_OPTIMISTIC_API_KEY}`,
   42161: `https://arb-mainnet.g.alchemy.com/v2/${process.env.REACT_APP_ARBITRUM_MAIN_KEY}`,
+  8453: `https://base-mainnet.g.alchemy.com/v2/${process.env.REACT_APP_ARBITRUM_MAIN_KEY}`,
   // testnet
   5: `https://eth-goerli.alchemyapi.io/v2/${process.env.REACT_APP_ALCHEMY_KEY}`,
   420: `https://opt-goerli.g.alchemy.com/v2/${process.env.REACT_APP_OPTIMISTIC_API_KEY}`,
@@ -42,6 +45,7 @@ const scans = {
   1: 'https://etherscan.io/address/',
   10: 'https://optimistic.etherscan.io/address/',
   42161: 'https://arbiscan.io/address/',
+  8453: 'https://basescan.org/address/',
  
   5: 'https://goerli.etherscan.io/address/',
   420: 'https://goerli-optimism.etherscan.io/address/',
@@ -109,6 +113,7 @@ const tokens = {
     5: '0x6C68a114c406dA77c3Ce1Ad1Cf09B420E37087b8',// '0x760389840E5B30919D881feb747C12054CBE671f'
     10: '0xB88a5Ac00917a02d82c7cd6CEBd73E2852d43574',
     420: '',
+    8453: '0xB88a5Ac00917a02d82c7cd6CEBd73E2852d43574',
     42161: '0xB88a5Ac00917a02d82c7cd6CEBd73E2852d43574',
     421613: '0xa5120a12Ff848b2e96439557A9f7E4083f921314',
   },
@@ -117,6 +122,7 @@ const tokens = {
     5: '0x504C094E10DC89100803b1A69b619c69751Dc9Ae',
     10: '0x89B1e7068bF8E3232dD8f16c35cAc45bDA584f4E',
     420: '',
+    8453: '0x89B1e7068bF8E3232dD8f16c35cAc45bDA584f4E',
     42161: '0x89B1e7068bF8E3232dD8f16c35cAc45bDA584f4E',
     421613: '0x98d06DBb715e16dB57021eCA85b44e7916EB0c17'
   },
@@ -125,6 +131,7 @@ const tokens = {
     5: '0x07865c6E87B9F70255377e024ace6630C1Eaa37F',
     10: '0x0b2c639c533813f4aa9d7837caf62653d097ff85',
     420: '',
+    8453: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913', // !?
     42161: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
     421613: '0x8FB1E3fC51F3b789dED7557E680551d93Ea9d892'
   }
@@ -136,6 +143,7 @@ const contracts = {
     5: '0x1655D8EC4d34BAE3E93a0864166676B86B0287d3',
     10: '0xae851449e85b30b104e3155e5f4bb1bfd3a97010',
     420: '',
+    8453: '0x7560d15774499386B04A64177E090B33e803493F',
     42161: '0xA014cCE13ECB3d92BB6D253b74Bb6E7Ed2418276',
     421613: '0x953b290385d856303834aCab13Ee12Bf2CEEF253'
   }
@@ -157,6 +165,10 @@ const AMMLinks = {
   420: {
     title: 'Swap on Optimism',
     link: 'https://app.balancer.fi/#/optimism/swap'
+  },
+  8453: {
+    title: 'Swap on Base',
+    link: 'https://app.balancer.fi/#/base/swap'
   },
   42161: {
     title: 'Swap on Arbitrum',
@@ -228,6 +240,12 @@ const mainChainList = [
     netId: 111,
     name: 'Optimism',
     logo: optIcon
+  },
+  {
+    chainId: 8453,
+    netId: 184,
+    name: 'Base',
+    logo: baseIcon
   },
 ]
 
