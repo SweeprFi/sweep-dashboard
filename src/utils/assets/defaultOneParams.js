9d324fd84c16e720a7f9c430e1cabebe99ebc4c3
Cela Pablo <pablooandress9@gmail.com>
2023-11-30 00:54:17 -0300

    removes custom asset files - adds default function file

--- src/utils/assets/defaultOneParams.js
+++ src/utils/assets/defaultOneParams.js
@@ -1,4 +1,4 @@
-export const structureOne = [
+export const defaultOneParams = [
   {
     name: "deposit",
     description: "Transfers of a specified amount to the asset. This must be a common transaction by the ERC20.",
@@ -14,6 +14,74 @@ export const structureOne = [
     ]
   },
   {
+    name: "borrow",
+    description: "Enables the borrowing of a specific amount of Sweep.",
+    params: [
+      {
+        name: "sweepAmount",
+        description: "Quantity of the token to be borrowed (i.e 1000000000000000000 is 1 SWEEP)."
+      }
+    ]
+  },
+  {
+    name: "sell Sweep On AMM",
+    description: "Sells sweep amount from the Stabilizer's balance to the AMM (swaps SWEEP to USDC).",
+    params: [
+      {
+        name: "sweepAmount",
+        description: "Amount to be changed in the AMM (i.e 1000000000000000000 is 1 SWEEP)."
+      },
+      {
+        name: "amountOutMin",
+        description: "The minimum amount of the target token that the function should obtain."
+      }
+    ]
+  },
+  {
+    name: "invest",
+    description: "Facilitates an investment operation in the asset.",
+    params: [
+      {
+        name: "usdxAmount",
+        description: "Amount to be invested (i.e 1000000 is 1 USDx)."
+      }
+    ]
+  },
+  {
+    name: "divest",
+    description: "Facilitates an divestment operation in the asset.",
+    params: [
+      {
+        name: "usdxAmount",
+        description: "Amount to be divested (i.e 1000000 is 1 USDx)."
+      }
+    ]
+  },
+  {
+    name: "buy Sweep On AMM",
+    description: "Buys sweep from the Stabilizer's balance to the AMM (swaps USDC to SWEEP).",
+    params: [
+      {
+        name: "usdxAmount",
+        description: "Amount to be changed in the AMM (i.e 1000000 is 1 USDx)."
+      },
+      {
+        name: "amountOutMin",
+        description: "The minimum amount of the target token that the function should obtain."
+      }
+    ]
+  },
+  {
+    name: "repay",
+    description: "Burns the Sweep amount to reduce the debt.",
+    params: [
+      {
+        name: "sweepAmount",
+        description: "Amount to be burnt by Sweep (i.e 1000000000000000000 is 1 SWEEP)."
+      }
+    ]
+  },
+  {
     name: "withdraw",
     description: "Facilitates the withdrawal of a specified amount  from the asset.",
     params: [
@@ -24,74 +92,6 @@ export const structureOne = [
       {
         name: "amount",
         description: "Quantity of the token to be withdrawn."
-      }
-    ]
-  },
-  {
-    name: "borrow",
-    description: "Enables the borrowing of a specific amount of Sweep.",
-    params: [
-      {
-        name: "sweepAmount",
-        description: "Quantity of the token to be borrowed."
-      }
-    ]
-  },
-  {
-    name: "repay",
-    description: "Burns the Sweep amount to reduce the debt.",
-    params: [
-      {
-        name: "sweepAmount",
-        description: "Amount to be burnt by Sweep."
-      }
-    ]
-  },
-  {
-    name: "invest",
-    description: "Facilitates an investment operation in the asset.",
-    params: [
-      {
-        name: "usdxAmount",
-        description: "Amount to be invested."
-      }
-    ]
-  },
-  {
-    name: "divest",
-    description: "Facilitates an divestment operation in the asset.",
-    params: [
-      {
-        name: "usdxAmount",
-        description: "Amount to be divested."
-      }
-    ]
-  },
-  {
-    name: "buy Sweep On AMM",
-    description: "Buys sweep from the Stabilizer's balance to the AMM (swaps USDC to SWEEP).",
-    params: [
-      {
-        name: "usdxAmount",
-        description: "Amount to be changed in the AMM."
-      },
-      {
-        name: "amountOutMin",
-        description: "The minimum amount of the target token that the function should obtain."
-      }
-    ]
-  },
-  {
-    name: "sell Sweep On AMM",
-    description: "Sells sweep amount from the Stabilizer's balance to the AMM (swaps SWEEP to USDC).",
-    params: [
-      {
-        name: "sweepAmount",
-        description: "Amount to be changed in the AMM."
-      },
-      {
-        name: "amountOutMin",
-        description: "The minimum amount of the target token that the function should obtain."
       }
     ]
   },
