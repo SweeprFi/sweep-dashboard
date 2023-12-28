1723f0975672960646bd708ff2f85a5337d7d0ac
petry26 <jose26petry@gmail.com>
2023-12-28 11:40:52 -0300

    added unavailable page

--- src/modules/routes.js
+++ src/modules/routes.js
@@ -2,11 +2,13 @@ import Home from "@pages/Home"
 import Dashboard from "@pages/Dashboard"
 import Asset from "@pages/Asset"
 import NotFound from "@pages/NotFound"
+import Unavailable from "@pages/Unavailable"
 
 const routes =  [
   { element: Home,      path: "/" },
   { element: Dashboard, path: "/dashboard/:network" },
   { element: Asset,     path: "/asset/:network/:address" },
+  { element: Unavailable,     path: "/unavailable" },
 
   { element: NotFound,  path: "*" },
 ];
