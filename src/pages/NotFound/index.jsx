69f135d04bdb22fe93f3d0b4a0ef5ae576c10799
petry26 <jose26petry@gmail.com>
2023-12-28 16:36:16 -0300

    removed absolute attribute from the main containers used on the notfound and unavailable page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -3,7 +3,7 @@ import { languages } from "@config/languages";
 
 const PageNotFound = () => {
   return (
-    <div className="flex justify-center items-center w-full h-full absolute top-0 left-0">
+    <div className="flex justify-center items-center w-full h-full top-0 left-0">
       <div className="flex flex-col items-center">
         <h1 className="text-white text-4xl sm:text-5xl lg:text-6xl font-bold uppercase font-nidus_sans__reguar tracking-widest">
           {languages.text_page_not_found}
