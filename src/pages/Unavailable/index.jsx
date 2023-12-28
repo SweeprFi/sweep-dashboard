90508475b249e2a0e3ff27944b37736884311b76
petry26 <jose26petry@gmail.com>
2023-12-28 18:57:41 -0300

    centered text in the unavailable page

--- src/pages/Unavailable/index.jsx
+++ src/pages/Unavailable/index.jsx
@@ -4,7 +4,7 @@ const PageUnavailable = () => {
   return (
     <div className="flex justify-center items-center w-full h-full top-0 left-0">
       <div className="flex flex-col items-center">
-        <h1 className="text-white text-4xl sm:text-5xl lg:text-6xl font-bold uppercase font-nidus_sans__reguar tracking-widest">
+        <h1 className="text-center text-white text-4xl sm:text-5xl lg:text-6xl font-bold uppercase font-nidus_sans__reguar tracking-widest">
           {languages.text_page_unavailable}
         </h1>
       </div>
